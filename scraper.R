7868beb99172f3fc32aeffdaaac2b02355b1b76b
Carlin Starrs <cstarrs@rms.com>
2024-08-12 14:56:39 -0700

    no idea if this will work

---
+++ scraper.R
@@ -0,0 +1,14 @@
+library(httr2)
+req <- request("https://newsapi.org/v2/everything") |>
+  req_url_query(
+    q = '`"data science"`',
+    from = Sys.Date() - 1,
+    pageSize = 10,
+    apiKey = Sys.getenv("NEWS_API_KEY")
+  )
+
+resp <- req_perform(req)
+df <- resp_body_json(resp)
+
+exportJSON <- jsonlite::toJSON(df)
+write(exportJSON, glue::glue("logs/log.json"))
